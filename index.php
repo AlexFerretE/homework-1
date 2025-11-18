05d2dea59753ffc3558f667f13defcab65c8d905
Ferret Alex <axope8@gmail.com>
2025-11-18 08:39:23 +0400

    change index.php v5

--- index.php
+++ index.php
@@ -5,3 +5,42 @@ $a = 'Рыба';
 $b = 'человек';
 
 echo "$a рыбою сыта, а $b человеком";
+
+// Задание №3: знакомство с документацией
+
+$variable = 3.14;
+// $variable = 3;
+// $variable = 'one';
+// $variable = true;
+// $variable = null;
+// $variable = [];
+
+if (is_bool($variable)) {
+    $type = 'bool';
+} elseif (is_float($variable)) {
+    $type = 'float';
+} elseif (is_int($variable)) {
+    $type = 'int';
+} elseif (is_string($variable)) {
+    $type = 'string';
+} elseif (is_null($variable)) {
+    $type = 'null';
+} else {
+    $type = 'other';
+}
+//  Ваш программный код, в котором переменной $type
+//  присваивается одно из значений: bool, float, 
+//  int, string, null или other
+
+echo "type is $type";
+
+// с помощью switch
+
+switch (gettype($variable)) {
+    case 'boolean': $type = 'bool'; break;    
+    case 'double': $type = 'float'; break;
+    case 'integer': $type = 'int'; break;
+    case 'string': $type = 'string'; break;
+    case 'NULL': $type = 'null'; break;
+    default: $type = 'other';
+}
